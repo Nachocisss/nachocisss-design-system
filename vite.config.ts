5b935a093e33798854ab3920cf3ae8dfed644804
Ignacio Cisternas <nachocisternas95@gmail.com>
2025-04-09 22:38:40 -0400

    including dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Crear __filename y __dirname para módulos ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -12,7 +10,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.jsx'),  // Usamos el archivo index
+      entry: path.resolve(__dirname, 'src/index.jsx'),
       name: 'DesignSystem',
       formats: ['es', 'umd'],
       fileName: (format) => `design-system.${format}.js`
